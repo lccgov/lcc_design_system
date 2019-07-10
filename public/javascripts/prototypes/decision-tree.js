ae6d2b401ad2c9b0144ecae0aba76a5a85951fb9
Kelly Clarkson <kelly.clarkson@leeds.gov.uk>
2019-07-10 17:01:57 +0100

    Amend decision tree content and made element accessible

--- public/javascripts/prototypes/decision-tree.js
+++ public/javascripts/prototypes/decision-tree.js
@@ -126,3 +126,157 @@
  
 })( jQuery );
 
+// Council tax - Changes to circumstances
+var data = {
+    message: '<h3>Do you want to tell us about a change?</h3>',
+    decisions: [
+        {
+            answer: 'Yes',
+            message: '<h3>Do you want to tell us about a move?</h3>',
+            decisions: [                
+                {
+                    answer: "Yes",
+                    message: '<h3>Are you a student?</h3>',     
+                    decisions: [                
+                        {
+                            answer: "Yes",
+                            message: '<h3>Council tax for students</h3><p>Visit our <a href="/council-tax/discounts-and-exemptions/students">Student discounts and exemptions</a> page to apply for student discount or to let us know about a change to your student status.</p>',                     
+                        },
+                        {
+                            answer: 'No',
+                            message: '<h3>Do you recieve benefits?</h3>',
+                            decisions: [
+                                {
+                                    answer: 'Yes',
+                                    message: '<h3>Council tax for people with benefits</h3><p>If you receive Housing Benefit, Council Tax Support or Free School meals you must tell us about any changes in your family\'s circumstances by visiting the <a href="/residents/council-tax-and-benefits/changes-in-circumstances">Council Tax Benefits Changes in Circumstances</a> page.</p>'
+                                },
+                                {
+                                    answer: "No",
+                                    message: '<h3>Where are you moving?</h3>',
+                                    decisions: [                
+                                        {
+                                            answer: "Out of Leeds",
+                                            message: '<h3>Are you moving out of a propery that will remain occupied?</h3>',  
+                                            decisions: [                
+                                                {
+                                                    answer: "Yes",
+                                                    message: '<h3>Moving out of a property that will remain occupied</h3><p>You will need to call our Council Tax Service to notify them you are moving (phone number to be confirmed).</p><p>Make sure you are removed from any tenancy agreement as you may still be liable.</p><p>You will need to notify your new council that you are moving.</p>',                     
+                                                },
+                                                {
+                                                    answer: 'No',
+                                                    message: '<h3>Are you currently a homeowner or renter?</h3>',
+                                                    decisions: [    
+                                                        {
+                                                            answer: 'Homeowner',
+                                                            message: '<h3>Homeowner moving out of Leeds</h3><p>You will be charged up until the date the sale completes.</p><p>You will need to notify your new council that you are moving.</p><p><h3>Have you sold the property you are leaving?</h3>',
+                                                                decisions: [
+                                                                    {
+                                                                        answer: 'Yes',
+                                                                        message: '<h3>Sold the property you are leaving</h3><p><a rel="external" class="btn btn-primary" href="#">Change of address form</a>'
+                                                                    },
+                                                                    {
+                                                                        answer: 'No',
+                                                                        message: '<h3>Empty properties</h3><p>You will need to notify us if your property becomes empty by visiting <a href="/council-tax/discounts-and-exemptions/empty-properties">Empty properties</a>.</p>',
+                                                                    }
+                                                                ]
+                                                        },        
+                                                        {
+                                                            answer: "Renter",
+                                                            message: '<h3>Are you currently on a rolling tenancy?</h3>', 
+                                                            decisions: [                
+                                                                {
+                                                                    answer: "Yes",
+                                                                    message: '<h3>Moving out of Leeds and still on a rolling tenancy</h3><p>If you are on a rolling tenancy you will be charged until you move.</p><p>You will need to notify your new council that you are moving.</p><p><a class="btn btn-primary" href="#" rel="external">Change of address form</a></p>',                     
+                                                                },
+                                                                {
+                                                                    answer: 'No',
+                                                                    message: '<h3>Moving out of Leeds and not on a rolling tenancy</h3><p>If you move out before your tenancy ends you will still be liable.</p><p>You will need to notify your new council that you are moving.</p><p><a class="btn btn-primary" href="#" rel="external">Change of address form</a></p>',
+                                                                }
+                                                            ]                     
+                                                        }                                                        
+                                                    ] 
+                                                }
+                                            ]                   
+                                        },
+                                        {
+                                            answer: 'Within Leeds',
+                                            message: '<h3>Moving within Leeds</h3><p>You will get a closing bill for the address you are moving out of and an opening bill for your new address.</p><h3>Are you moving out of a property that will remain occupied?</h3>',
+                                            decisions: [
+                                                {
+                                                    answer: 'Yes',
+                                                    message: '<h3>Moving out of a property that will remain occupied</h3><p>You will need to call our Council Tax service to notify them you are moving.</p><p>Make sure you are removed from any tenancy agreement as you may still be liable (number to be confirmed).</p>',
+                                                },
+                                                {
+                                                    answer: 'No',
+                                                    message: '<h3>Are you moving into a property occupied by an existing bill payer?</h3>',
+                                                    decisions: [
+                                                        {
+                                                            answer: 'Yes',
+                                                            message: '<h3>Moving into a property occupied by an existing bill payer</h3><p>You will need to call our Council Tax service to notify them you are moving.</p><p>Make sure you are removed from any tenancy agreement as you may still be liable (number to be confirmed).</p>',
+                                                        },
+                                                        {
+                                                            answer: 'No',
+                                                            message: '<h3>Recently built properties</h3><p>If you are moving into a property that has recently been built you may need to check that this has been banded.</p> <a href="https://www.leeds.gov.uk/council-tax/bands-and-charges" rel="external">Check your property band</a> <h3>Are you moving from a property you rented or owned?</h3>',
+                                                            decisions: [
+                                                                {
+                                                                    answer: 'Rented',
+                                                                    message: '<h3>Moving from a rented property</h3><p><a class="btn btn-primary" href="#" rel="external">Change of address form</a>'
+                                                                },
+                                                                {
+                                                                    answer: 'Owned',
+                                                                    message: '<h3>Have you sold the property you are leaving?</h3>',
+                                                                    decisions: [
+                                                                        {
+                                                                            answer: 'Yes',
+                                                                            message: '<h3>Sold the property you are leaving</h3><p><a rel="external" class="btn btn-primary" href="#">Change of address form</a>'
+                                                                        },
+                                                                        {
+                                                                            answer: 'No',
+                                                                            message: '<h3>Empty properties</h3><p>You will need to notify if your property becomes empty by visiting <a href="/council-tax/discounts-and-exemptions/empty-properties">Empty properties</a>.</p>',
+                                                                        }
+                                                                    ]
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+
+                                            ]
+                                        },
+                                        {
+                                            answer: 'Into Leeds',
+                                            message: '<h3>Will you be renting or owning your home in Leeds?</h3>',
+                                            decisions: [                
+                                                {
+                                                    answer: "Renting",
+                                                    message: '<h3>Moving into Leeds and renting</h3><p>You will be charged from when your tenancy starts.</p><p><a class="btn btn-primary" href="#" rel="external">Change of address form</a></p>'                                                                      
+                                                },
+                                                {
+                                                    answer: 'Homeowner',
+                                                    message: '<h3>Moving into Leeds and will be a homeowner</h3><p>You will be charged from your completion date.</p><p>If your property is a new build you will need to check that this has been banded.<p><a href="https://www.leeds.gov.uk/council-tax/bands-and-charges" rel="external">Check your bands property</a></p><p><a class="btn btn-primary" href="#" rel="external">Change of address form</a></p>',
+                                                }
+                                            ] 
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]                   
+                },
+                {
+                    answer: 'No',
+                    message: '<h3>Report a different change</h3><p>Visit our <a href="/council-tax/report-a-change">Report a change</a> page to amend a direct debit, tell us about a change of name or circumstance or report any other changes.</p>'
+                }
+            ]            
+        },
+        {
+            answer: 'No',
+            message: '<h3>Report a different change</h3><p>Visit our <a href="/council-tax/report-a-change">Report a change</a> page to amend a direct debit, tell us about a change of name or circumstance or report any other changes.</p>'
+            
+        }            
+    ]
+};
+
+jQuery(document).ready(function() {
+    jQuery('.lcc-council-tax-tree').decisionTree({data: data});
+});
