8333fda95053f822d1b8da1ae27f31db34e8d4dd
Ben Hodge <benjamin.hodge@leeds.gov.uk>
2019-10-11 14:22:02 +0100

    adding select conditional

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -35,6 +35,17 @@ if($('.form-group--radio').length > 0) {
     });
 }
 
+// Select - other
+if( $('.form-control').length > 0 ) {
+    $('select').on('change', function() {
+        $(this).parent().removeClass('active');
+        var value = this.value;
+        if( value == 'Other' ) {
+            $(this).parent().addClass('active');
+        }
+    });
+}
+
 
 
 //input block label
